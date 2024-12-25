6d05ca583ec7188e780d71b3c8c80011a6a229ee
ABK <ABK@DESKTOP-PTCS16K>
2024-12-25 12:07:45 +0300

    Add project files.

---
+++ Dec25Task/Models/Demo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec25Task.Models
+{
+    internal class Demo
+    {
+        private string vUsername;
+
+        public string Username
+        {
+            get { return vUsername; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    vUsername = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter username");
+
+                }
+            }
+        }
+        
+        public void GetUsername()
+        {
+            Console.WriteLine("My username is " + vUsername);
+        }
+       
+        
+        
+        private int vSalary;
+        public int Salary
+        {
+            get
+            {
+                return vSalary;
+            }
+            set
+            {
+                if (Salary >= 350 && Salary <= 900)
+                {
+                    vSalary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary is out of range");
+                }
+            }
+
+        }
+    }
+}
+
+
---
+++ Dec25Task/Program.cs
@@ -0,0 +1,9 @@
+﻿using Dec25Task.Models;
+Demo info = new Demo();
+
+Console.WriteLine("Enter Username");
+info.Username = Console.ReadLine();
+info.GetUsername();
+
+Console.WriteLine("Enter Salary");
+info.Salary = Convert.ToInt32(Console.ReadLine());
